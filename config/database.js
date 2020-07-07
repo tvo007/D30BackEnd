c7eac0434662e418c4e67d1019ed4d0e8fa96c67
Tim Vo <timtvo714@gmail.com>
2020-07-06 21:42:43 -0700

    cloudinary provider

--- config/database.js
+++ config/database.js
@@ -18,3 +18,18 @@ module.exports = ({ env }) => ({
 });
 
 
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
